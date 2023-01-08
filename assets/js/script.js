058e8f5ccfb3b86f5420a2e6c2d7c729275c5a6d
thorne7 <darienhallam@gmail.com>
2023-01-08 23:45:03 +1030

    start/timer function

--- assets/js/script.js
+++ assets/js/script.js
@@ -26,3 +26,28 @@ var questions = [
         answer: 
     }
 ]; 
+
+var score = 0;
+var timer = 80;
+
+document.getElementById("start").addEventListener("click", startQuiz);
+
+var quizContainer = document.querySelector('.quizContainer')
+var timeContainer = document.getElementById('time')
+
+function startQuiz() {
+    quizContainer.innerHTML = ""
+    startTimer()
+    displayQuestionOne()
+}
+
+function startTimer() {
+    timeContainer.textContent = timer
+    var setTime = setInterval(function() {
+        if (timer <= 1) {
+            clearInterval(setTime)
+        }
+        timer--
+        timeContainer.textContent = timer
+    }, 1000)
+}
