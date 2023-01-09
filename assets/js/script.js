5a16a430e5b2c03aff521a9828cd85205cd00892
thorne7 <darienhallam@gmail.com>
2023-01-10 00:28:45 +1030

    fixing high score button

--- assets/js/script.js
+++ assets/js/script.js
@@ -10,21 +10,21 @@ var questions = [
         
     //2
     {
-        question: '',
-        options: '',
-        answer: '',
+        question: 'A local scope mean that a variable is decalred within a/an...',
+        options: [ 'for Loop', 'function', '.js file', 'Array' ],
+        answer: 'function',
     },
     //3
     {
-        question: '',
-        options: '',
-        answer: '',
+        question: 'the first character beginning in a Array is ... ',
+        options: [ '1', 'A', '0', 'a' ],    
+        answer: '0',
     },
     //4        
     {
-        question: '',
-        options: '',
-        answer: '',
+        question: 'What events can an Event Listener handle',
+        options: ['Click', 'Key Up', 'Double Click', 'Key Down', 'All of the Above'],
+        answer: 'All of the Above',
     }
 ]; 
 
@@ -32,9 +32,12 @@ var score = 0;
 var timer = 80;
 
 document.getElementById("start").addEventListener("click", startQuiz);
+document.getElementById("highscores").addEventListener("click", highscorebtn);
 
 var quizContainer = document.querySelector('.quizContainer')
 var timeContainer = document.getElementById('time')
+// var highscoreContainer = document.getElementById('highscore')
+var storage = JSON.parse(localStorage.getItem('highscore'))
 
 function startQuiz() {
     quizContainer.innerHTML = ""
@@ -79,4 +82,127 @@ function displayQuestionOne() {
     }
 }
 
+function displayQuestionTwo() {
+    quizContainer.innerHTML = ""
+    var questionEl = document.createElement('h1')
+    questionEl.textContent = questions[1].question
+    quizContainer.append(questionEl)
 
+    var optionsContainer = document.createElement('ul')
+    quizContainer.append(optionsContainer)
+    for (var i = 0; i < questions[1].options.length; i++) {
+        var listItems = document.createElement('li')
+        listItems.setAttribute('id', questions[1].options[i])
+        listItems.textContent = questions[1].options[i]
+        optionsContainer.append(listItems)
+
+        listItems.addEventListener('click', function (event) {
+            if (event.target.id === questions[1].answer) {
+                console.log('correct');
+                score += 20
+            } else {
+                console.log('incorrect');
+                timer -= 10
+            }
+            displayQuestionThree()
+        })
+    }
+}
+
+function displayQuestionThree() {
+    quizContainer.innerHTML = ""
+    var questionEl = document.createElement('h1')
+    questionEl.textContent = questions[2].question
+    quizContainer.append(questionEl)
+
+    var optionsContainer = document.createElement('ul')
+    quizContainer.append(optionsContainer)
+    for (var i = 0; i < questions[2].options.length; i++) {
+        var listItems = document.createElement('li')
+        listItems.setAttribute('id', questions[2].options[i])
+        listItems.textContent = questions[2].options[i]
+        optionsContainer.append(listItems)
+
+        listItems.addEventListener('click', function (event) {
+            if (event.target.id === questions[2].answer) {
+                console.log('correct');
+                score += 20
+            } else {
+                console.log('incorrect');
+                timer -= 10
+            }
+            displayQuestionFour()
+        })
+    }
+}
+
+function displayQuestionFour() {
+    quizContainer.innerHTML = ""
+    var questionEl = document.createElement('h1')
+    questionEl.textContent = questions[3].question
+    quizContainer.append(questionEl)
+
+    var optionsContainer = document.createElement('ul')
+    quizContainer.append(optionsContainer)
+    for (var i = 0; i < questions[3].options.length; i++) {
+        var listItems = document.createElement('li')
+        listItems.setAttribute('id', questions[3].options[i])
+        listItems.textContent = questions[3].options[i]
+        optionsContainer.append(listItems)
+
+        listItems.addEventListener('click', function (event) {
+            if (event.target.id === questions[3].answer) {
+                console.log('correct');
+                score += 20
+            } else {
+                console.log('incorrect');
+                timer -= 10
+            }
+            endQuiz()
+        })
+    }
+}
+// fixing storage & high score 
+function endQuiz() {
+    quizContainer.textContent = 'Quiz is over your final score is: ' + score
+
+    var input = document.createElement('input')
+    input.setAttribute('placeholder', 'What is you name?')
+    quizContainer.append(input)
+
+    var btn = document.createElement('button')
+    btn.textContent = 'Submit'
+    quizContainer.append(btn)
+
+    btn.addEventListener('click', function() {
+        var storage = JSON.parse(localStorage.getItem('highscore'))
+        if(storage === null) {
+            storage = []
+        }
+
+        var user = {
+            name: input.value,
+            currentScore: score
+        }
+
+        storage.push(user)
+        localStorage.setItem('highscore', JSON.stringify(storage))
+        highscorebtn
+    })
+
+};
+
+function highscorebtn() {
+    var storage = JSON.parse(localStorage.getItem('highscore'))
+
+    if (storage === null) {
+        highscore.textContent = 'No Highscores'
+    } else {
+        highscore.textContent = ''
+        for(var i = 0; i < storage.length; i++) {
+            var p = document.createElement('p')
+            p.textContent = 'Name: ' + storage[i].name + ' ----- Score: ' + storage[i].currentScore 
+            highscore.append(p)
+        }
+    }
+}
