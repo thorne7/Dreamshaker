3e3273b88b81abc9e4dee0761504e5f3f47016ed
thorne7 <darienhallam@gmail.com>
2023-01-09 22:47:33 +1030

    first question function

--- assets/js/script.js
+++ assets/js/script.js
@@ -2,10 +2,12 @@
 var questions = [
     //1
     {
-        question: '',
-        options: '',
-        answer: '',
+        question: 'Which of these is requied to create a string',
+        options: [ '{}','()','""','<>'],
+        answer: '" "'
+       
     },
+        
     //2
     {
         question: '',
@@ -50,3 +52,31 @@ function startTimer() {
         timeContainer.textContent = timer
     }, 1000)
 }
+
+function displayQuestionOne() {
+    var questionEl = document.createElement('h1')
+    questionEl.textContent = questions[0].question
+    quizContainer.append(questionEl)
+
+    var optionsContainer = document.createElement('ul')
+    quizContainer.append(optionsContainer)
+    for (var i = 0; i < questions[0].options.length; i++) {
+        var listItems = document.createElement('li')
+        listItems.setAttribute('id', questions[0].options[i])
+        listItems.textContent = questions[0].options[i]
+        optionsContainer.append(listItems)
+
+        listItems.addEventListener('click', function (event) {
+            if (event.target.id === questions[0].answer) {
+                console.log('correct');
+                score += 20
+            } else {
+                console.log('incorrect');
+                timer -= 10
+            }
+            displayQuestionTwo()
+        })
+    }
+}
+
+
